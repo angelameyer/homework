11bef1216c826549e3d90707d555906973b12ac2
Angela Meyer <angela.td.meyer@gmail.com>
2013-09-27 10:21:00 -0700

    added cat_flash

---
+++ cat_flash/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatFlash::Application.config.secret_key_base = '04705dda40c574b0562d1b716df6d54e6c9f2a71fee6c8f19e1ff992ac9f4b14bcbb351633d994cb965adf8536b2611846b7db3f491008dbaffc95e1243a049c'
